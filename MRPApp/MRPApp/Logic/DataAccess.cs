91e724f853df30b66b79b7fddc2bf86b318d1ad8
guemin96 <guemin12@naver.copm>
2021-06-24 12:42:50 +0900

    20210624오전수업끝

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,25 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        //셋팅 테이블에서 데이터 가져오기
+        internal static List<Settings> GetSettings()
+        {
+            List<Model.Settings> settings;
+            using (var ctx = new MRPEntities()) // ctx에 MRP데이터를 넣는 행동 , new MRPEntities를 사용함으로써 커넥션 커맨드 파라미터 역할을 해줌
+            {
+                settings = ctx.Settings.ToList();
+            }// using을 사용함으로써 데이터베이스를 열고 알아서 닫아주는 역할을 함
+            return settings;
+
+        }
+
+        internal static int SetSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);
+                return ctx.SaveChanges(); //COMMIT
+            }
+        }
     }
 }
--- MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
+++ MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MRPApp.View.Setting
 {
@@ -28,13 +29,21 @@ namespace MRPApp.View.Setting
         {
             try
             {
-                
+                LoadGridData();
+                LblBasicCode.Visibility = LblCodeDesc.Visibility = LblCodeName.Visibility = Visibility.Hidden;
+
             }
             catch (Exception ex)
             {
                 Commons.LOGGER.Error($"예외발생 StoreList Loaded : {ex}");
                 throw ex;
             }
+        }
+
+        private void LoadGridData()
+        {
+            List<Model.Settings> settings = Logic.DataAccess.GetSettings();
+            this.DataContext = settings;
         }
 
         private void BtnEditUser_Click(object sender, RoutedEventArgs e)
@@ -86,6 +95,96 @@ namespace MRPApp.View.Setting
         private void BtnExportExcel_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            var setting = new Model.Settings();
+            setting.BasicCode = TxtBasicCode.Text;
+            setting.CodeName = TxtCodeName.Text;
+            setting.CodeDesc = TxtCodeDesc.Text;
+            try
+            {
+                var result = Logic.DataAccess.SetSettings(setting);
+                if (result == 0)
+                {
+                    Commons.LOGGER.Error("데이터 수정시 오류 발생");
+                    await Commons.ShowMessageAsync("오류", "데이터 입력실패!!");//등록일 수정하기 다음시간!
+                }
+                else
+                {
+                    Commons.LOGGER.Info($"데이터 수정 성공 : {setting.BasicCode}");
+                    ClearInputs();
+                    LoadGridData();//수정된 값 다시 불러오는 역할
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 {ex}");
+            }
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var setting = GrdData.SelectedItem as Model.Settings;
+            setting.CodeName = TxtCodeName.Text;
+            setting.CodeDesc = TxtCodeDesc.Text;
+            try
+            {
+                var result = Logic.DataAccess.SetSettings(setting);
+                if (result == 0)
+                {
+                    Commons.LOGGER.Error("데이터 수정시 오류 발생");
+                    Commons.ShowMessageAsync("오류", "데이터 수정실패!!");
+                }
+                else
+                {
+                    Commons.LOGGER.Info($"데이터 수정 성공 : {setting.BasicCode}");
+                    ClearInputs();
+                    LoadGridData();//수정된 값 다시 불러오는 역할
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 {ex}");
+            }
+        }
+
+        private void ClearInputs()
+        {
+            TxtBasicCode.IsReadOnly = false;
+            TxtBasicCode.Background = new SolidColorBrush(Colors.White);
+            TxtBasicCode.Text = TxtCodeDesc.Text = TxtCodeName.Text = string.Empty;//""
+        }
+
+        private void GrdData_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            try
+            {
+                var setting = GrdData.SelectedItem as Model.Settings;
+                TxtBasicCode.Text = setting.BasicCode;
+                TxtCodeDesc.Text = setting.CodeDesc;
+                TxtCodeName.Text = setting.CodeName;
+
+                TxtBasicCode.IsReadOnly = true;
+                TxtBasicCode.Background = new SolidColorBrush(Colors.LightGray);
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외 발생 : {ex}");
+            }
         }
     }
 }
